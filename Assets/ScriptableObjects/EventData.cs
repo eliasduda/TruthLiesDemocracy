f43ef59a280ef1cc5209097c73f76d6c64093504
Elias <elias.duda21@gmail.com>
2025-09-26 19:55:40 +0200

    Event stuff

---
+++ Assets/ScriptableObjects/EventData.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EventData", menuName = "Scriptable Objects/EventData")]
+public class EventData : ScriptableObject
+{
+    public string eventName;
+
+    public string description;
+
+    public Sprite icon;
+
+    public EventCategory category;
+
+    public EventEffect[] effects;
+
+    public EventEffectPair cost;
+}
