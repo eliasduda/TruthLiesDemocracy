e6923cca019d6360cf00a6259aaa1b4a66a0adb4
Yorollimor <53179730+Yorollimor@users.noreply.github.com>
2025-09-27 13:18:24 +0200

    UI Setup update

--- Assets/Scripts/DayPassed.cs
+++ Assets/Scripts/DayPassed.cs
@@ -1,11 +1,8 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(AudioSource))]
 public class DayPassed : MonoBehaviour
 {
-    public AudioClip dayPassedSound;
-    public AudioSource audioSource;
     public Image dayPassed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -14,9 +11,8 @@ public class DayPassed : MonoBehaviour
         dayPassed.enabled = false;
     }
 
-    public void SetDayActive()
+    public void PassDay()
     {
         dayPassed.enabled = true;
-        AudioSource.PlayClipAtPoint(dayPassedSound, Camera.main.transform.position);
     }
 }
