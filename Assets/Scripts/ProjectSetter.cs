0830d231a4c4b90ecffa156bf4401dc86f2572df
Elias <elias.duda21@gmail.com>
2025-09-28 16:16:48 +0200

    fix screen text replace

--- Assets/Scripts/ProjectSetter.cs
+++ Assets/Scripts/ProjectSetter.cs
@@ -10,13 +10,12 @@ public class ProjectSetter : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        projectTitle = GetComponent<TextMeshProUGUI>();
         projectTitle.text = projectTitle.text.Replace("NAME", GameManager.instance.gamePlaySettings.playerName);
         projectTitle.text = projectTitle.text.Replace("PROJECT", GameManager.instance.gamePlaySettings.projectName);
-        winScreen.text = projectTitle.text.Replace("NAME", GameManager.instance.gamePlaySettings.playerName);
-        winScreen.text = projectTitle.text.Replace("PROJECT", GameManager.instance.gamePlaySettings.projectName);
-        looseScreen.text = projectTitle.text.Replace("NAME", GameManager.instance.gamePlaySettings.playerName);
-        looseScreen.text = projectTitle.text.Replace("PROJECT", GameManager.instance.gamePlaySettings.projectName);
+        winScreen.text = winScreen.text.Replace("NAME", GameManager.instance.gamePlaySettings.playerName);
+        winScreen.text = winScreen.text.Replace("PROJECT", GameManager.instance.gamePlaySettings.projectName);
+        looseScreen.text = looseScreen.text.Replace("NAME", GameManager.instance.gamePlaySettings.playerName);
+        looseScreen.text = looseScreen.text.Replace("PROJECT", GameManager.instance.gamePlaySettings.projectName);
     }
 
     // Update is called once per frame
